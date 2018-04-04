4db1c83e6535fc7b24aa52984e2e294198ccdfa9
Vinit Kumar <Kumar_Vinit@cat.com>
2018-04-04 14:44:28 +0530

    added resources

--- src/services/loader.service.ts
+++ src/services/loader.service.ts
@@ -43,7 +43,7 @@ export class LoaderService implements HttpInterceptor {
         console.log('show');
         this.loader = this.loadingCtrl.create({
             content: 'Please wait...',
-            duration: 2000
+            duration: 5000
         });
         this.loader.present();
     }
